96db601f093f54985c0212d8350fa11d75ec1d88
Samantha <samantha@dbas-Mac-mini.local>
2022-11-29 18:19:57 -0800

    added api variables

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,4 @@
-console.log()
+var apiKey = "46e193ff5a178d02cba4c8f7f600438b"
+var today = dayjs().format("l");
+var city;
+var queryUrl = "https://api.openweathermap.org/data/2.5/weather?q={city}&units=imperial&appid={apiKey}";
