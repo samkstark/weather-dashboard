cbd68ed8e723b2be4950b613babc3de04e6f7284
Samantha <samantha@dbas-Mac-mini.local>
2022-12-02 13:04:31 -0800

    added more java for api call

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,20 @@
-var apiKey = "46e193ff5a178d02cba4c8f7f600438b"
-var today = dayjs().format("l");
-var city;
-var queryUrl = "https://api.openweathermap.org/data/2.5/weather?q={city}&units=imperial&appid={apiKey}";
+var apiKey = "46e193ff5a178d02cba4c8f7f600438b";
+var today = new Date();
+console.log(today);
+var city = document.getElementById("searchCity");
+var queryUrl =
+  "https://api.openweathermap.org/data/2.5/weather?q={city}&units=imperial&appid={apiKey}";
+//api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}
+
+document.getElementById("searchBtn").addEventListener("click", function () {
+    fetch(
+        `https://api.openweathermap.org/data/2.5/forecast?q=${city.value}&units=metric&appid=46e193ff5a178d02cba4c8f7f600438b`
+      )
+        .then(function (data) {
+          console.log(data);
+          return data.json();
+        })
+        .then(function (weatherData) {
+          console.log(weatherData.list[0].main.temp);
+        });
+});
